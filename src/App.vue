be3e4362634d122bb70b03fd6aa49a80febc8053
Deepika <gotlad1@hawkmail.newpaltz.edu>
2019-04-11 09:35:22 -0400

    login

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div>
     <Nav />
     <div class="container" id="main">
+     <div class="alert alert-warning alert-dismissible fade show" role="alert">
+       <button type="button" class="Close" aria-label="Close">
+         <spam aria-hidden="true">&times;</spam>
+         </button>
+         <strong>{{e.msg}}</strong>
+         </div>
+
       <router-view/>
     </div>
   </div>
@@ -9,6 +16,7 @@
 
 <script>
 import Nav from "@/components/Nav";
+import { Globals } from "@/models/api";
 export default {
   components: {
     Nav
---
+++ src/components/api.js
@@ -0,0 +1,29 @@
+const API_ROOT = process.env.API_ROOT || "http://localhost:3000/";
+
+export const Globals = {
+    user: null,
+    errors: [],
+    deleteError(i){
+        this.errors.splice(i, 1);
+    }
+}
+
+export function login(){
+    Globals.user = { name: "Bernie" }
+}
+
+export function api(url, data){
+    if(!data){
+        return fetch(API_ROOT + url).then(x=> x.json());
+    }else{
+        return fetch(API_ROOT + url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
+        .then(response => response.json()); // parses JSON response into native Javascript objects
+    }
+}
